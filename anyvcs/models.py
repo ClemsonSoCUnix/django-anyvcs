a37e4421b8e3824aee29d2ab96e1579b1db4cd48
Scott Duckworth <sduckwo@clemson.edu>
2013-08-08 16:30:16 -0400

    implement access controls for subversion

--- anyvcs/models.py
+++ anyvcs/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.db.models.signals import post_save, post_delete
-from django.dispatch import receiver
 from django.contrib.auth.models import User, Group
 from django.core.exceptions import ValidationError
 from . import settings
@@ -53,6 +52,14 @@ class Repo(models.Model):
       else:
         assert False, self.vcs
       subprocess.check_call(cmd)
+    if self.vcs == 'svn':
+      conf_path = os.path.join(self.path, 'conf', 'svnserve.conf')
+      with open(conf_path, 'w') as conf:
+        conf.write('[general]\n')
+        d = { '': 'none', 'r': 'read', 'rw': 'write' }
+        conf.write('anon-access = %s\n' % d[self.public_rights])
+        conf.write('authz-db = authz\n')
+      self.update_authz()
 
   def post_delete(self, **kwargs):
     shutil.rmtree(self.path)
@@ -73,6 +80,21 @@ class Repo(models.Model):
         msg = 'Invalid name'
         raise ValidationError({'name': [msg]})
 
+  def update_authz(self):
+    if self.vcs == 'svn':
+      authz_path = os.path.join(self.path, 'conf', 'authz')
+      with open(authz_path, 'w') as authz:
+        authz.write('[groups]\n')
+        for gr in self.grouprights_set.all():
+          members = ','.join((u.username for u in gr.group.user_set.all()))
+          authz.write('@%s = %s\n' % (gr.group.name, members))
+        authz.write('\n[/]\n')
+        for ur in self.userrights_set.all():
+          authz.write('%s = %s\n' % (ur.user.username, ur.rights))
+        for gr in self.grouprights_set.all():
+          authz.write('@%s = %s\n' % (gr.group.name, gr.rights))
+        authz.write('* = %s\n' % self.public_rights)
+
 class UserRights(models.Model):
   repo = models.ForeignKey(Repo, db_index=True)
   user = models.ForeignKey(User, db_index=True)
@@ -85,6 +107,12 @@ class UserRights(models.Model):
 
   def __unicode__(self):
     return u'%s/%s' % (self.repo, self.user)
+
+  def post_save(self, created, **kwargs):
+    self.repo.update_authz()
+
+  def post_delete(self, **kwargs):
+    self.repo.update_authz()
 
 class GroupRights(models.Model):
   repo = models.ForeignKey(Repo, db_index=True)
@@ -99,10 +127,26 @@ class GroupRights(models.Model):
   def __unicode__(self):
     return u'%s/%s' % (self.repo, self.group)
 
-@receiver(post_save, sender=Repo, dispatch_uid=__name__)
+  def post_save(self, created, **kwargs):
+    self.repo.update_authz()
+
+  def post_delete(self, **kwargs):
+    self.repo.update_authz()
+
 def post_save_proxy(sender, instance, **kwargs):
   instance.post_save(**kwargs)
 
-@receiver(post_delete, sender=Repo, dispatch_uid=__name__)
 def post_delete_proxy(sender, instance, **kwargs):
   instance.post_delete(**kwargs)
+
+# Repo signals
+post_save.connect(post_save_proxy, dispatch_uid=__name__, sender=Repo)
+post_delete.connect(post_delete_proxy, dispatch_uid=__name__, sender=Repo)
+
+# UserRights signals
+post_save.connect(post_save_proxy, dispatch_uid=__name__, sender=UserRights)
+post_delete.connect(post_delete_proxy, dispatch_uid=__name__, sender=UserRights)
+
+# GroupRights signals
+post_save.connect(post_save_proxy, dispatch_uid=__name__, sender=GroupRights)
+post_delete.connect(post_delete_proxy, dispatch_uid=__name__, sender=GroupRights)

0e5beecee20e9d17084fcd1b048cc9654016cf03
Scott Duckworth <sduckwo@clemson.edu>
2013-08-30 13:14:31 -0400

    add license info

--- anyvcs/models.py
+++ anyvcs/models.py
@@ -1,3 +1,20 @@
+# Copyright 2013 Scott Duckworth
+#
+# This file is part of django-anyvcs.
+#
+# django-anyvcs is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# django-anyvcs is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with django-anyvcs.  If not, see <http://www.gnu.org/licenses/>.
+
 from django.db import models
 from django.db.models import Q
 from django.db.models.signals import post_save, pre_delete, post_delete

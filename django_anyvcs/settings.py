435cf7d5b41b386e7af7dc23101a986887ec9671
Scott Duckworth <sduckwo@clemson.edu>
2013-09-27 11:54:51 -0400

    remove unused settings

--- django_anyvcs/settings.py
+++ django_anyvcs/settings.py
@@ -21,6 +21,3 @@ VCSREPO_ROOT = settings.VCSREPO_ROOT
 VCSREPO_HOSTS_ALLOW = getattr(settings, 'VCSREPO_HOSTS_ALLOW', ('127.0.0.1', '::1', '::ffff:127.0.0.1'))
 VCSREPO_HOSTS_ALLOW_FUNCTION = getattr(settings, 'VCSREPO_HOSTS_ALLOW_FUNCTION', None)
 VCSREPO_RIGHTS_FUNCTION = getattr(settings, 'VCSREPO_RIGHTS_FUNCTION', None)
-GIT = getattr(settings, 'GIT', 'git')
-HG = getattr(settings, 'HG', 'hg')
-SVNADMIN = getattr(settings, 'SVNADMIN', 'svnadmin')

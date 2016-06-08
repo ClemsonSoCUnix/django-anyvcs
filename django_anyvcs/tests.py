b6821e560928011ad213c76fd179de2bbe62f365
Paul Kilgo <pkilgo@clemson.edu>
2016-06-08 09:55:39 -0400

    Update the copyright year

--- django_anyvcs/tests.py
+++ django_anyvcs/tests.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2014-2015, Clemson University
+# Copyright (c) 2014-2016, Clemson University
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
